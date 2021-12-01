8a1ad4e7637c9f060ed8747ca8215245446b6d1b
kubus199115 <jakubmarciniak@o2.pl>
2021-12-01 21:22:19 +0100

    create dto

---
+++ src/main/java/com/example/invoiceJavaBackend/dto/ItemDTO.java
@@ -0,0 +1,101 @@
+package com.example.invoiceJavaBackend.dto;
+
+import java.math.BigDecimal;
+
+public class ItemDTO {
+    
+    private String name;
+
+    private String pkwiu;
+
+    private Integer quantity;
+
+    private String unit;
+
+    private BigDecimal netPrice;
+
+    private BigDecimal netValue;
+
+    private BigDecimal taxRate;
+
+    private BigDecimal taxValue;
+
+    private BigDecimal grossValue;
+
+    public ItemDTO() {
+        
+    }
+
+    public BigDecimal getGrossValue() {
+        return grossValue;
+    }
+
+    public void setGrossValue(BigDecimal grossValue) {
+        this.grossValue = grossValue;
+    }
+
+    public BigDecimal getTaxValue() {
+        return taxValue;
+    }
+
+    public void setTaxValue(BigDecimal taxValue) {
+        this.taxValue = taxValue;
+    }
+
+    public BigDecimal getTaxRate() {
+        return taxRate;
+    }
+
+    public void setTaxRate(BigDecimal taxRate) {
+        this.taxRate = taxRate;
+    }
+
+    public BigDecimal getNetValue() {
+        return netValue;
+    }
+
+    public void setNetValue(BigDecimal netValue) {
+        this.netValue = netValue;
+    }
+
+    public BigDecimal getNetPrice() {
+        return netPrice;
+    }
+
+    public void setNetPrice(BigDecimal netPrice) {
+        this.netPrice = netPrice;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPkwiu() {
+        return pkwiu;
+    }
+
+    public void setPkwiu(String pkwiu) {
+        this.pkwiu = pkwiu;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
